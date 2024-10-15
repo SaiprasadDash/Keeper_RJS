f66daaa549a6634350324a4fc0078c5b392f5264
Sai prasad Dash <134990873+SaiprasadDash@users.noreply.github.com>
2024-10-15 11:20:40 +0530

    Add files via upload

---
+++ component/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function Footer() {
+    return (
+        <footer>
+        <p>copyright &copyright Sai prasad Dash</p>
+        </footer>
+    );
+}
+export default Footer;
